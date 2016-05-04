cb386785996020ee450c1c73b4f8149e04856d09
Michelle McManus <michelleisclever@gmail.com>
2016-05-04 18:52:15 -0500

    rspec add

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pins#index'
 
-  resources :pins
+  get "pins/name-:slug" => "pins#show_by_name"
+    
+    resources :pins
     
     get '/library' => 'pins#index'
+    
     
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
