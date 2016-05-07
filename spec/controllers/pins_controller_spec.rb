d2c9a9bcc0b244d06403a2cfd7eef15b17a7eb08
Michelle McManus <michelleisclever@gmail.com>
2016-05-06 20:44:32 -0500

    Adding name route for show_by_name route

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe PinsController do
         get :index
         expect(response).to render_template("index")
     end
+        
+        it 'populates @pins with all pins' do
+            get :index
+            expect(assigns[:pins]).to eq(Pin.all)
+end
 end
 end
